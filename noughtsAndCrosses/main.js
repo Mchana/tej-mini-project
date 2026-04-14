82c5dd0d324800ae3c2d2265c6480ef060c42ef1
Mchana <muntejchana@hotmail.com>
2026-04-14 13:32:18 +0100

    some changes idk

--- noughtsAndCrosses/main.js
+++ noughtsAndCrosses/main.js
@@ -4,17 +4,18 @@ let player = 1
 
 
 function on_click(event) {
-    let cell = event.target
-    let cellID = Number(event.target.id)
+    console.log(board)
+    let cell = event.currentTarget
+    let cellID = Number(cell.id)
 
-    if (board[cellID] !==0) return;
+   if (board[cellID] === 0) return
 
-    if (player === 1 && board[cellID] === 0){
+    if (player === 1){
         cell.textContent = "X"
         board[cellID] = 1
         player = 2
 
-    } else if (player === 2 && board[cellID] === 0){
+    } else if (player === 2){
         cell.textContent = "O"
         board[cellID] = 2
         player = 1
@@ -23,23 +24,35 @@ function on_click(event) {
 
 };
 function sumBoard (board) {
-    hasWon(board[0]+board[1]+board[2])
-    hasWon(board[3]+board[4]+board[5])
-    hasWon(board[6]+board[7]+board[8])
-    hasWon(board[0]+board[3]+board[6])
-    hasWon(board[1]+board[4]+board[7])
-    hasWon(board[2]+board[5]+board[8])
-    hasWon(board[0]+board[4]+board[8])
-    hasWon(board[2]+board[4]+board[6])
+        let lines = [
+            (board[0]+board[1]+board[2]),
+        (board[3]+board[4]+board[5]),
+        (board[6]+board[7]+board[8]),
+        (board[0]+board[3]+board[6]),
+        (board[1]+board[4]+board[7]),
+        (board[2]+board[5]+board[8]),
+        (board[0]+board[4]+board[8]),
+        (board[2]+board[4]+board[6])
+    ]
+
+    for (let line of lines){
+            if (hasWon(line) === true) {
+                break
+            }
+    }
+
 }
 
 function hasWon (sum) {
     if (sum === 3) {
         console.log ("player 1 has won")
+        return true
     } else if (sum === 6) {
         console.log ("player 2 has won")
+        return true
 
     }
+    return false
 }
 
 // const returnXOrO = (player) =>{
