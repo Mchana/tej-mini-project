2ee67fb9e8afe6350e38ea0643ec83c2af03bf5c
Mchana <muntejchana@hotmail.com>
2026-04-14 11:11:36 +0100

    changing files names to standard

--- noughtsAndCrosses/main.js
+++ noughtsAndCrosses/main.js
@@ -1,13 +1,23 @@
 let board = [1,2,3,4,5,6,7,8,9];
 let win = false;
+let player = 1
 console.log(board)
 
-
-const cell = document.getElementById("id")
-
-const onClick = (cell) =>{
+while (win = false){
 
 }
+
+const on_click = () => {
+    let cell = document.getElementById("id")
+
+    if (player === 1){
+        cell.textContent = "X"
+        player = 2
+    } else if (player === 2){
+        cell.textContent = "O"
+        player = 1
+    }
+};
 
 const sumBoard = (board) => {
     hasWon(board[0]+board[1]+board[2])
@@ -19,7 +29,6 @@ const sumBoard = (board) => {
     hasWon(board[0]+board[4]+board[8])
     hasWon(board[2]+board[4]+board[6])
 }
-// sum columns = 3n
 
 const hasWon = (sum) => {
     if (sum === 3) {
@@ -30,10 +39,10 @@ const hasWon = (sum) => {
     }
 }
 
-const returnXOrO = (turn) =>{
-    if (turn === 1) {
+const returnXOrO = (player) =>{
+    if (player === 1) {
         return "X"
-    } else if (turn === 2) {
+    } else if (player === 2) {
         return "O"
     }
 }
